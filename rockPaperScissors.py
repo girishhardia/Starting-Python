8211ac79c5c575529a65095a6c68cc8d05b67026
girishhardia <girishhardia@gmail.com>
2022-10-29 13:20:41 +0530

    game program

---
+++ rockPaperScissors.py
@@ -0,0 +1,21 @@
+import random
+user_input = input("Enter a choise: (rock, paper or scissors)  ")
+possible_actions = ["rock", "paper","scissors"]
+computer_action = random.choice(possible_actions)
+
+print("Computer chose",computer_action)
+
+if user_input==computer_action:
+    print("Draw")
+elif user_input == "rock" and computer_action == "paper":
+    print("You Lose")
+elif user_input == "rock" and computer_action == "scissors":
+    print("You Won")
+elif user_input == "paper" and computer_action == "rock":
+    print("You Lose")
+elif user_input == "paper" and computer_action == "scissors":
+    print("You Won")
+elif user_input == "scissors" and computer_action == "rock":
+    print("You Lose")
+elif user_input == "scissors" and computer_action == "paper":
+    print("You Won")
