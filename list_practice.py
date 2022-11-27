ba78d2f42a3fc8a78084336e0247badb61d829ff
girishhardia <girishhardia@gmail.com>
2022-11-27 23:44:38 +0530

    practice

---
+++ list_practice.py
@@ -0,0 +1,42 @@
+colors = ["red", "Blue", "Red", "Black"]
+cars = ["TATA", "NANO", "ALTO", "JEEP"]
+numbers = [1,4,9,6,2,10]
+
+# cars.sort()
+# numbers.sort(reverse=True)
+# print(numbers)
+# print(cars)
+
+# new_list = [x.lower() for x in cars]
+# print(new_list)
+
+# [print(x) for x in cars]
+
+# new_list = []
+# for i in cars:
+#    if "A" in i:
+#         new_list.append(i)
+# print(new_list)
+
+# new_list = [x for x in cars if x != "TATA"]
+# print(new_list)
+
+# for x in range(len(cars)):
+#     print(cars[x])
+
+# x = 0
+# while x < len(cars):
+#     print(cars[x])
+#     x += 1
+
+# num1 = list(numbers)
+
+list1 = ["x", "y", "z"]
+list2 = [1,2,3]
+
+# list3 = list1 + list2
+# print(list3)
+# list1.extend(list2)
+# print(list1)
+# list1.append(list2)
+# print(list1)
