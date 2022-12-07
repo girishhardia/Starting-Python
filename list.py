96a480913bddfed2d3a0818aef52aa3bf9175ecc
girishhardia <girishhardia@gmail.com>
2022-12-07 19:04:55 +0530

    Lists

---
+++ list.py
@@ -0,0 +1,23 @@
+list1 = ["Phil","Oz","Seuss","Dre"]
+# append()	Adds an element at the end of the list
+# clear()	Removes all the elements from the list
+# copy()	Returns a copy of the list
+# count()	Returns the number of elements with the specified value
+# extend()	Add the elements of a list (or any iterable), to the end of the current list
+# index()	Returns the index of the first element with the specified value
+# insert()	Adds an element at the specified position
+# pop()	Removes the element at the specified position
+# remove()	Removes the first item with the specified value
+# reverse()	Reverses the order of the list
+# sort()	Sorts the list
+
+# list1.append("Oz")
+# list1.clear()
+# print(list1)
+
+# tupple = {"oye", "toy"}
+# list1.extend(tupple)
+# print(list1)
+
+# list1.sort(reverse=True)
+# print(list1)
---
+++ list_practice2.py
@@ -0,0 +1,19 @@
+# numbers = [1,2,3,4,5]
+
+# square_numbers = []
+# for i in numbers:
+#     j = i**2
+#     square_numbers.append(j)
+# print(square_numbers)
+
+
+
+numbers = [1,2,3,4,5,2,6]
+
+numbers.insert(numbers.index(2),200)
+numbers.remove(2)
+print(numbers)
+#list are used to store multiple non_homogenous data in a single variable 
+#changeble
+#ordered
+#allows duplicate value
