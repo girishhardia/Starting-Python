71134bb94cb452a19d6724f2d98e60d71e9fbbd2
girishhardia <girishhardia@gmail.com>
2022-12-10 00:29:44 +0530

    Concept

---
+++ recursion.py
@@ -0,0 +1,21 @@
+#Recursion
+def hello_world():
+    print("Hello World")
+    hello_world()
+hello_world()
+
+# def sum_recursion(num):
+#     if num == 0:
+#         return num
+#     return num+sum_recursion(num-1)
+# ans = sum_recursion(5)
+# print(ans)
+
+# def factorial(num):
+#     if num == 1:
+#         return num
+#     return num*factorial(num-1)
+
+# ans = factorial(1)
+# print(ans)
+
