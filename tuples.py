2d7cdae46d1ad8145798dd20c962c75cce0a780f
girishhardia <girishhardia@gmail.com>
2022-12-10 00:31:13 +0530

    Concept

---
+++ tuples.py
@@ -0,0 +1,55 @@
+#tuples stores multiple items in a single variable
+# ordered
+# unchangable
+# use () for tuples
+
+mytuple = ("apple", "oranges","pineapple")
+# print(mytuple)``
+# print(len(mytuple))   
+
+# a=list(mytuple)
+# a.append("grapes")
+# print(type(a))
+# print(a)
+# mytuple=tuple(a)
+# print(mytuple)
+
+# tuple1 = (1,2,3)
+# tuple2 = (4,5,6)
+
+# tuple1 += tuple2
+# print(tuple1)
+# del tuple2
+# # print(tuple2)
+# print(tuple1[::-1])
+
+# name = ["Girish"]
+# print(tuple(name))
+
+# tuple1 = (10,20,30,40)
+# (a,b,c,d) = tuple1
+# print(a,b,c,d,sep="\n")
+
+# tuple1 = (10,20)
+# tuple2 = (30,40)
+# # a = tuple1
+# # tuple1 = tuple2
+# # tuple2 = a
+# # print(tuple1)
+# # print(tuple2)
+# #       OR          
+# tuple1,tuple2 = tuple2,tuple1
+# print(tuple1)
+# print(tuple2)
+
+# i = 0
+# while i < len(mytuple):
+#     print(mytuple[i])
+#     i+=1
+
+# print(mytuple.count("apple"))
+
+# tuple1 = (1,[2,7],2,3,4,5,6,)
+# # print(tuple1[1][1])
+# tuple1[1][0]=9
+# print(tuple1)
