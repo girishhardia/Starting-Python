a0a69edcc528068bb92111c49f3168ef9a9e3b10
girishhardia <girishhardia@gmail.com>
2022-12-17 15:51:56 +0530

    concept

---
+++ funtionsIntro.py
@@ -0,0 +1,47 @@
+# def hello_world():
+#     print("Hello World")
+
+# def hello_mars():
+#     print("Hello Mars")
+
+# hello_mars()
+# hello_world()
+
+# list1 = [213,4455,78,684,4563,456,8543,54]
+# list2 = [59,349,439,495,545,4532]
+# def add_num(a,b):
+#     print(int(a)+int(b))
+
+# add_num(list1[0],list2[0])
+# for i in list1:
+#     for j in list2:
+#         add_num(i,j)
+
+# def multi(t,f):
+# # this is how you use return
+#     mul = t*f
+#     return mul
+# result = multi(2,3)
+# print(result)
+
+# def me(name, age):
+#     print("my name is",name,"and my age is",age)
+# me("Girish","19")
+
+# def company(name, salary=10000):
+#     print("Employee name:", name, "salary:",salary)
+    
+# company("Ben","12000")
+# company("Mike","15000")
+# company("Bob")
+
+# #Keyword Variable Length Argument
+# def details(name, **data):
+#     print(name)
+#     for i,j in data.items():
+#         print(i,j)
+# details("Girish", place = "Indore", age = 19, status = "single" , phno = 213478950)
+
+
+
+
