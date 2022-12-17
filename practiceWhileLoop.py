e5f36ba527f7c3d5e1e6c612f232ffa491fa9e0a
girishhardia <girishhardia@gmail.com>
2022-12-17 15:54:15 +0530

    practice

---
+++ practiceWhileLoop.py
@@ -0,0 +1,21 @@
+a = "*"
+while len(a)<10:
+    print(a)
+    a = a+"*"
+
+i = 1
+while i<=20:
+    if i%2==0:
+        print(i, "is even")
+    else:
+        print(i, "is odd")
+    i = i+1
+
+
+i = 20
+while i>0:
+    if i%2==0:
+        print(i, "is even")
+    else:
+        print(i, "is odd")
+    i = i-1
